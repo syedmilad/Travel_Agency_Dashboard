35a2c77a82ff32ea121bd11ed194c61716879c7d
syedmilad <syedmilad99@gmail.com>
2025-06-04 15:01:21 +0500

    implement routing, dashboard and auth using app writer

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,4 +1,8 @@
-import { ChipDirective, ChipListComponent, ChipsDirective } from "@syncfusion/ej2-react-buttons";
+import {
+  ChipDirective,
+  ChipListComponent,
+  ChipsDirective,
+} from "@syncfusion/ej2-react-buttons";
 import { getFirstWord } from "lib/utils";
 import { Link, useLocation } from "react-router";
 
@@ -21,6 +25,7 @@ const TripCard = ({
       className="trip-card"
     >
       <img src={imageUrl} alt={name} />
+      
       <article>
         <h2>{name}</h2>
         <figure>
@@ -35,20 +40,23 @@ const TripCard = ({
 
       <div className="mt-5 pl-[18px] pr-3.5 pb-6">
         <ChipListComponent>
-
           <ChipsDirective>
-            {tags.map((tag,index)=>(
+            {tags.map((tag, index) => (
               <ChipDirective
                 key={tag}
                 text={getFirstWord(tag)}
-                cssClass={`${index === 1 ? "!bg-pink-50 !text-pink-500" : "!bg-suceessp-50 !text-success-700"}`}
+                cssClass={`${
+                  index === 1
+                    ? "!bg-pink-50 !text-pink-500"
+                    : "!bg-suceessp-50 !text-success-700"
+                }`}
               />
             ))}
           </ChipsDirective>
         </ChipListComponent>
       </div>
 
-            <article className="tripCard-pill">{price}</article>
+      <article className="tripCard-pill">{price}</article>
     </Link>
   );
 };
